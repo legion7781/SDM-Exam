6f44e2e64d4d2c0b4f3911eb4d9b54ba3f085aee
omkar wani <omkarwani7781>
2023-02-02 11:29:13 +0530

    First Version of Project

---
+++ PsudoProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Omkar's Legion
+ *
+ */
+module PsudoProject {
+}
---
+++ PsudoProject/src/test/OddEven.java
@@ -0,0 +1,17 @@
+package test;
+public class OddEven {
+
+	public static boolean isEven(int num)
+	{
+		if(num%2==0)
+			return true;
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isEven(23));
+
+	}
+
+}
